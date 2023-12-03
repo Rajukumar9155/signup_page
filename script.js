b9f5b55279d69db9d073d01ecc2b59fc97607dca
Rajukumar9155 <rajukumar683.hitece2020@gmail.com>
2023-12-03 23:06:34 +0530

    nothing

--- script.js
+++ script.js
@@ -19,3 +19,19 @@ btnPopup.addEventListener('click', () => {
 iconClose.addEventListener('click', () => {
     wrapper.classList.remove('active-popup');
 });
+
+/*This is for opening and closing hamburger */
+let hamburger=document.getElementsByClassName('hamburger')[0];
+let hamburger_close=document.getElementsByClassName('hamburger-close')[0];
+let navigation = document.getElementsByClassName('navigation')[0];
+console.log(navigation);
+function openNav(){
+    navigation.style.display = "flex";
+    hamburger.style.display="none";
+    hamburger_close.style.display = "block";
+}
+function closeNav(){
+    navigation.style.display = "none";
+    hamburger.style.display="block";
+    hamburger_close.style.display = "none";
+}
